f26b8251551f93a14fd656c4e82fdc86fa3b8de3
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-01-24 16:52:07 +0530

    Create Example1.go

---
+++ Example1.go
@@ -0,0 +1,9 @@
+package unit_test
+
+// CheckDivisibility checks if a number is divisible by 5 and returns a string
+func CheckDivisibility(input int) string {
+    if input%5 == 0 {
+        return "FIVE"
+    }
+    return "NOT FIVE"
+}
